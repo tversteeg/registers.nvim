552bca0acba243eac36bd58a9c26089b8d5ea194
Thomas Versteeg <thomas@versteeg.email>
2021-02-12 18:11:43 +0100

    Automatically size the window to the amount of filled registers

--- lua/registers.lua
+++ lua/registers.lua
@@ -51,19 +51,54 @@ local function register_contents(register_name)
 	return vim.api.nvim_exec(("echo getreg(%q)"):format(register_name), true)
 end
 
+-- Build a map of all the lines
+local function read_registers()
+	register_lines = {}
+
+	-- Loop through all the types
+	for _, reg_type in ipairs(register_map) do
+		-- Loop through the separate registers of the type
+		for _, reg in ipairs(reg_type.registers) do
+			-- The contents of a register
+			local contents = register_contents(reg)
+
+			-- Skip empty registers
+			if #contents > 0 then
+				-- Display the whitespace of the line as whitespace
+				contents = contents:gsub("\t", "\\t")
+				-- Newlines have to be replaced
+				:gsub("[\n\r]", "⏎")
+
+				-- Get the line with all the information
+				local line = string.format("%s: %s", reg, contents)
+
+				-- Truncate the line
+				register_lines[#register_lines + 1] = {
+					register = reg,
+					line = line,
+				}
+			end
+		end
+	end
+end
+
 -- Spawn a popup window
 local function open_window()
+	-- Read all the registers
+	read_registers()
+
 	-- Create empty buffer
 	buf = vim.api.nvim_create_buf(false, true)
 
+	-- Remove the buffer when the window is closed
 	vim.api.nvim_buf_set_option(buf, 'bufhidden', 'wipe')
 
 	-- Get dimensions
 	local width = vim.api.nvim_get_option("columns")
 	local height = vim.api.nvim_get_option("lines")
 
-	-- Calculate our floating window size
-	local win_height = math.ceil(height * 0.8 - 4)
+	-- Calculate the floating window size
+	local win_height = math.min(#register_lines, math.ceil(height * 0.8 - 4))
 	local win_width = math.ceil(width * 0.8)
 
 	-- Set some options
@@ -86,46 +121,23 @@ end
 
 -- Update the popup window
 local function update_view()
-	local result = {}
-
 	-- Get the width of the window to truncate the strings
 	local max_width = vim.api.nvim_win_get_width(win) - 2
 
-	local line_number = 1
-	register_lines = {}
+	-- Create a array of lines from all the registers
+	local lines = {}
+	for i = 1, #register_lines do
+		local line = register_lines[i].line
 
-	-- Loop through all the types
-	for _, reg_type in ipairs(register_map) do
-		-- Loop through the separate registers of the type
-		for _, reg in ipairs(reg_type.registers) do
-			-- The contents of a register
-			local contents = register_contents(reg)
-
-			-- Skip empty registers
-			if #contents > 0 then
-				-- Display the whitespace of the line as whitespace
-				contents = contents:gsub("\t", "\\t")
-					-- Newlines have to be replaced
-					:gsub("[\n\r]", "⏎")
-
-				-- Get the line with all the information
-				local line = string.format("%s: %s", reg, contents)
-
-				-- Truncate the line
-				result[#result + 1] = line:sub(1, max_width)
-
-				-- Keep track of the line numbers
-				register_lines[reg] = line_number
-				line_number = line_number + 1
-			end
-		end
+		-- Truncate the line to the width of the window
+		lines[i] = line:sub(1, max_width)
 	end
 
 	-- Write the lines to the buffer
-	vim.api.nvim_buf_set_lines(buf, 0, -1, false, result)
+	vim.api.nvim_buf_set_lines(buf, 0, -1, false, lines)
 
 	-- Add the highlights
-	for i = 1, line_number do
+	for i = 1, #register_lines do
 		vim.api.nvim_buf_add_highlight(buf, -1, "RegistersRegisterChar", i - 1, 0, 1)
 		vim.api.nvim_buf_add_highlight(buf, -1, "RegistersString", i - 1, 3, -1)
 	end
@@ -143,31 +155,39 @@ end
 local function apply_register(register)
 	local sleep = true
 
-	-- If no register is passed use the currently selected line
-	if not register then
-		-- Get the currently selected line
-		local line = unpack(vim.api.nvim_win_get_cursor(win))
+	local line
 
-		-- Find the matching register line
-		for reg, line_number in pairs(register_lines) do
-			if line_number == line then
-				register = reg
-				-- Don't sleep when we select it ourselves
-				sleep = false
+	-- Try to find the line of the register
+	if not register then
+		-- If no register is passed use the currently selected line
+
+		-- Get the currently selected line
+		line = unpack(vim.api.nvim_win_get_cursor(win))
+
+		-- Don't sleep when we select it
+		if line <= #register_lines then
+			sleep = false
+		end
+	else
+		-- Find the matching register line and get the line number
+		for i, register_line in ipairs(register_lines) do
+			if register_line.register == register then
+				line = i
 				break
 			end
 		end
 	end
 
 	-- Move the cursor to the register selected if applicable
-	if register_lines and register_lines[register] then
-		vim.api.nvim_win_set_cursor(win, {register_lines[register], 0})
+	if line then
+		-- Move the cursor
+		vim.api.nvim_win_set_cursor(win, {line, 0})
 
 		-- Redraw so the line get's highlighted
 		vim.api.nvim_exec("silent! redraw", true)
 
 		if sleep then
-			-- Wait for a second before closing the window
+			-- Wait for some time before closing the window
 			vim.api.nvim_exec(("silent! sleep %d"):format(1), true)
 		end
 	end
