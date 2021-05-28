74e2a6120fc3078358bcbd4539948cf0ab7b0e26
Bob Qi <baobin.qi@fmr.com>
2021-05-28 16:07:44 +0800

    recover register after using

--- lua/registers.lua
+++ lua/registers.lua
@@ -278,8 +278,14 @@ local function apply_register(register)
 		if register == "=" then
 			vim.api.nvim_feedkeys(key .. "=", "n", true)
 		else
+			local old_expr_content = vim.fn.getreg('=', 1)
 			local submit = vim.api.nvim_replace_termcodes("<CR>", true, true, true)
+			-- let execute the selected register content using `=` register and insert the result
 			vim.api.nvim_feedkeys(key .. "=@" .. register .. submit, "n", true)
+			-- recover the `=` register
+			vim.defer_fn(function()
+				vim.fn.setreg('=', old_expr_content)
+			end, 100)
 		end
 	else
 		-- Define the keys pressed based on the mode
