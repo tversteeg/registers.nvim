bbe95a8777590d05e3ee5cc00e2ada1710881367
Gaute Ravndal <gaute.ravndal@gmail.com>
2022-08-03 00:48:54 +0200

    Add colon to ALL_REGISTERS

--- lua/registers.lua
+++ lua/registers.lua
@@ -11,7 +11,8 @@
 ---@field private _mappings table<string, function>
 local registers = {}
 
----@mod intro Introduction
+-- All available registers
+local ALL_REGISTERS = { "*", "+", "\"", "-", "/", "_", "=", "#", "%", ".", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", ":" }
 
 ---@brief [[
 ---Registers.nvim is a minimal but very configurable Neovim plugin.
