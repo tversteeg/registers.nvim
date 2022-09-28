7e47d1a04ce1418edea1cc0a8decdbf4dfa660f2
Thomas Versteeg <t@versteeg.email>
2022-09-28 09:56:17 +0200

    Make backwards compatibility detection Lua only

--- plugin/registers.lua
+++ plugin/registers.lua
@@ -1,35 +1,37 @@
-" This file is only for automatic detection of upgrades from 1.x versions, in future versions it will be removed
+-- This file is only for automatic detection of upgrades from 1.x versions, in future versions it will be removed
 
-" Prevent loading file twice
-if exists('g:registers_loaded') | finish | endif
+-- Prevent loading file twice
+if vim.g.registers_migration_loaded then
+    return
+end
+vim.g.registers_migration_loaded = true
 
-lua << EOF
 -- Keep track of all settings detected and how they should be replaced
 local settings_detected = {}
 
-function detect_config(old_name, new_type, mutate)
-	local ok, var = pcall(function()
-		return vim.api.nvim_get_var("registers_" .. old_name)
-	end)
-	-- If no var is found do nothing
-	if not ok then
-		return
-	end
+local function detect_config(old_name, new_type, mutate)
+    local ok, var = pcall(function()
+        return vim.api.nvim_get_var("registers_" .. old_name)
+    end)
+    -- If no var is found do nothing
+    if not ok then
+        return
+    end
 
-	-- Convert the var to Lua values
-	if new_type == "boolean" and var == 0 then
-		var = false
-	elseif new_type == "boolean" and var == 1 then
-		var = true
-	elseif new_type == "number" then
-		var = tonumber(var)
-	end
+    -- Convert the var to Lua values
+    if new_type == "boolean" and var == 0 then
+        var = false
+    elseif new_type == "boolean" and var == 1 then
+        var = true
+    elseif new_type == "number" then
+        var = tonumber(var)
+    end
 
-	-- Create a list with the example table that will be merged
-	local mergeable_config = mutate(var)
-	if mergeable_config then
-		settings_detected[#settings_detected + 1] = mergeable_config
-	end
+    -- Create a list with the example table that will be merged
+    local mergeable_config = mutate(var)
+    if mergeable_config then
+        settings_detected[#settings_detected + 1] = mergeable_config
+    end
 end
 
 detect_config("show", "string", function(new) return { show = new } end)
@@ -45,37 +47,34 @@ detect_config("tab_symbol", "string", function(new) return { symbols = { tab = n
 detect_config("space_symbol", "string", function(new) return { symbols = { space = new } } end)
 detect_config("return_symbol", "string", function(new) return { symbols = { newline = new } } end)
 detect_config("normal_mode", "boolean", function(new)
-	if new == false then
-		return { bind_keys = { normal = false } }
-	end
+    if new == false then
+        return { bind_keys = { normal = false } }
+    end
 end)
 detect_config("visual_mode", "boolean", function(new)
-	if new == false then
-		return { bind_keys = { visual = false } }
-	end
+    if new == false then
+        return { bind_keys = { visual = false } }
+    end
 end)
 detect_config("insert_mode", "boolean", function(new)
-	if new == false then
-		return { bind_keys = { insert = false } }
-	end
+    if new == false then
+        return { bind_keys = { insert = false } }
+    end
 end)
 
 if #settings_detected > 0 then
-	-- Merge all settings into a single map
-	local settings = {}
-	for _, merge in ipairs(settings_detected) do
-		settings = vim.tbl_extend("error", settings, merge)
-	end
+    -- Merge all settings into a single map
+    local settings = {}
+    for _, merge in ipairs(settings_detected) do
+        settings = vim.tbl_extend("error", settings, merge)
+    end
 
-	vim.notify(([[You have recently updated the plugin 'registers.nvim' to version 2.0 or later.
+    vim.notify(([[You have recently updated the plugin 'registers.nvim' to version 2.0 or later.
 	This version has a new configuration mechanism using a Lua function you'll need to call manually. Previous configuration is still found. To migrate add the following to `init.vim` or `init.lua`:
 
 	lua << EOF
 	local registers = require("registers")
 	registers.setup(%s)
 	EOF
-	]]):format(vim.inspect(settings)), vim.log.levels.WARN)
+	]]  ):format(vim.inspect(settings)), vim.log.levels.WARN)
 end
-EOF
-
-let g:registers_loaded = 1
